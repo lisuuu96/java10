9c80c435a6b3ffc50f0c918521a84bca9e88fa78
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-01 18:20:03 +0100

    one

--- js/main7.js
+++ js/main7.js
@@ -1,11 +1,10 @@
 let num1 = 2;
 let num2 = 5;
-function add(num1, num2) 
-{let result = num1 + num2;
-     return result;}
+function number (num1, num2) 
+{return num1>num2;}
      
- add(2,5);
+ number(2,5);
 
 
-console.log(add(2,5))
+console.log(number(2,5))
 
