fc3ea6aa820eab80f0d9dae34281c8da845da2ad
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-05 08:20:02 +0100

    changes

--- js/main7.js
+++ js/main7.js
@@ -1,10 +1,9 @@
 let num1 = 2;
 let num2 = 5;
-function number (num1, num2) 
-{return num1>num2;}
+
+function number (num1, num2) {
+    return num1>num2;
+}
      
- number(2,5);
-
-
 console.log(number(2,5))
 
