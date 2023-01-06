e43c0b13122d5dfc8d4d28f7d03aa5b8f43e68d9
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-06 16:38:46 +0100

    ch

--- js/main7.js
+++ js/main7.js
@@ -2,7 +2,11 @@ let num1 = 2;
 let num2 = 5;
 
 function number (num1, num2) {
-    return num1>num2;
+    if(num1>num2)
+    return num1;
+    else{
+        return num2 ;
+    }
 }
      
 console.log(number(2,5))
